7c3f653cb622d7c16a2b303559f77353214fa387
Mohammad Soqar <mnsoqar1@gmail.com>
2023-08-23 21:04:04 +0300

    JS files

--- js/show-on-scroll.js
+++ js/show-on-scroll.js
@@ -1,16 +1,19 @@
 // Detect request animation frame
 var scroll = window.requestAnimationFrame ||
-             // IE Fallback
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+  // IE Fallback
+  function(callback) {
+    window.setTimeout(callback, 1000/60);
+  };
+
+var elementsToShow = $('.show-on-scroll');
 
 function loop() {
-
-  elementsToShow.forEach(function (element) {
+  elementsToShow.each(function() {
+    var element = $(this);
     if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
+      element.addClass('is-visible');
     } else {
-      element.classList.remove('is-visible');
+      element.removeClass('is-visible');
     }
   });
 
@@ -22,19 +25,10 @@ loop();
 
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-  
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
+  var rect = el[0].getBoundingClientRect();
   return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
--- services-version2.js
+++ services-version2.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $(".service1").hover(
         function () {
-            $(this).find("h2").css({"margin-top": "0",});
-            $(this).css({"background-color": "#ff2e62","border-radius": "0.5rem"});
+            $(this).find("h3").css({ "margin-top": "0", });
+            $(this).css({ "background-color": "#ff2e62", "border-radius": "1rem" });
         },
         function () {
-            $(this).find("h2").css({"margin-top": "10rem" });
-            $(this).css({"background-color": "#262a35" , "border-radius": "0rem","disply":"none"});
+            $(this).find("h3").css({ "margin-top": "10rem" });
+            $(this).css({ "background-color": "#262a35", "border-radius": "0rem", "disply": "none" });
         }
     );
 });
